5bc149dd4139b55bb15092bba950433614e26110
Jonas Bak Phillipson <jbakchr@gmail.com>
2024-05-11 21:57:14 +0200

    Added show and tell example code

---
+++ lectures/evening-01/01-show-and-tell/02-multiple-lines-of-code/main.py
@@ -0,0 +1,8 @@
+"""
+First just execute this code.
+
+After show that then reverse the order of the two lines below in order to show how the order of execution works.
+"""
+
+print("Hello,")
+print("World!")
